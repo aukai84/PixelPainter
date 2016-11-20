7f5233cd2223063bbf697a2787f5752b42254f7f
Aukai Tirrell <mtirrell@hawaii.edu>
2016-11-20 10:22:01 -1000

    changed size and amount of pixels, trying to make canvas look legit

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -29,16 +29,16 @@ function clickedGrid() {
   grid.className = "grid";
   document.body.appendChild(grid);
 
-  for(var i = 0; i < 20; i++){
+  for(var i = 0; i < 60; i++){
     var tr = document.createElement('tr');
     grid.appendChild(tr);
 
-    for(var j = 0; j < 20; j++){
+    for(var j = 0; j < 60; j++){
       var cell = document.createElement('td');
       cell.className = "clickCells";
       tr.appendChild(cell);
       cell.addEventListener("click", setColor);
-      cell.innerHTML = "cell";
+
     }
   }
 return grid;
