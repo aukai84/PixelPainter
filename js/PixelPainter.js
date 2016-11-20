53df84631f57c6de0777837c576eb8eed828cbdc
Andrea <takamiya.andrea@gmail.com>
2016-11-19 18:15:03 -1000

    Added a color grid, added some borders

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,5 +1,5 @@
 
-function createGrid() {
+function clickedGrid() {
   // Test change by Andrea
   var grid = document.createElement('table');
   grid.className = "grid";
@@ -11,10 +11,33 @@ function createGrid() {
 
     for(var j = 0; j < 20; j++){
       var cell = document.createElement('td');
+      cell.className = "clickCells";
       tr.appendChild(cell);
       cell.innerHTML = "test";
     }
   }
+
 return grid;
 
-} createGrid();
+} clickedGrid();
+
+function colorGrid(){
+
+  var colorGrid = document.createElement('table');
+  colorGrid.className = "colorGrid";
+  document.body.appendChild(colorGrid);
+
+  for(var i = 0; i < 10; i++){
+    var tr = document.createElement('tr');
+    colorGrid.appendChild(tr);
+
+    for(var j = 0; j < 5; j++){
+      var cell = document.createElement('td');
+      cell.className = "colorCells";
+      tr.appendChild(cell);
+      cell.innerHTML = "colors";
+    }
+   }
+    return colorGrid;
+
+} colorGrid();
